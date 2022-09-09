e2c7d1c5b3cff6f04b7d3856c5a37338c6b9430b
Aleksei Berdnikov <Aleksei240398@gmail.com>
2022-09-09 19:02:06 +0400

    Изменена ссылка на товар для проверки отсутствия хардкода значений

--- test_product_page.py
+++ test_product_page.py
@@ -5,7 +5,7 @@ import time
 
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/" + \
-            "the-shellcoders-handbook_209/?promo=newYear"
+            "coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     NameProduct = page.search_name_product()
